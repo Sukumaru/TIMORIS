b472e0f05415564afa22119aac49f0b65b7e3f31
Shu Zhou <shuzh@umich.edu>
2025-02-10 10:37:13 -0500

    Update test_spatial_analysis.py

--- tests/test_spatial_analysis.py
+++ tests/test_spatial_analysis.py
@@ -1,8 +1,11 @@
 import numpy as np
 from src.detection.spatial_analysis import compute_nearest_neighbor_distances
 
+import numpy as np
+from src.detection.spatial_analysis import compute_nearest_neighbor_distances
+
 def test_compute_nearest_neighbor_distances():
     coords = np.array([[0, 0], [1, 1], [2, 2]])
     distances = compute_nearest_neighbor_distances(coords)
-    assert len(distances) == len(coords),
-    assert all(d > 0 for d in distances), 
+    assert len(distances) == len(coords), "Output should match number of input points"
+    assert all(d > 0 for d in distances), "All distances should be greater than 0"
