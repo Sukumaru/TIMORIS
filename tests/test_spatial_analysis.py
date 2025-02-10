e5f81a77d8ff36d602f499b8c845cc52db52a089
Shu Zhou <shuzh@umich.edu>
2025-02-10 10:44:55 -0500

    Fixed NameError by adding missing sys import

--- tests/test_spatial_analysis.py
+++ tests/test_spatial_analysis.py
@@ -1,8 +1,11 @@
-import numpy as np
+import sys
+import os
+
+# Ensure the 'src' directory is in the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from src.detection.spatial_analysis import compute_nearest_neighbor_distances
-
+from detection.spatial_analysis import compute_nearest_neighbor_distances
+import numpy as np
 
 def test_compute_nearest_neighbor_distances():
     coords = np.array([[0, 0], [1, 1], [2, 2]])
