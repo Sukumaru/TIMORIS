b5ee947068650c5cca6aa3bcde9e845f97df16c0
Shu Zhou <shuzh@umich.edu>
2025-02-23 19:02:01 -0500

    Update __init__.py

--- src/timoris/__init__.py
+++ src/timoris/__init__.py
@@ -2,4 +2,4 @@ from .spatial_graph import generate_spatial_graph
 from .spatial_analysis import detect_spatial_patterns
 from .data_preprocessing import preprocess_data
 
-__all__ = ["generate_spatial_graph", "detect_spatial_patterns", "preprocess_data"]
+__all__ = ["generate_spatial_graph", "compute_nearest_neighbor_distances", "preprocess_data"]
